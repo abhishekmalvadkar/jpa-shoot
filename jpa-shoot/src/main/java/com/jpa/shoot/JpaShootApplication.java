93faaad0875887fe8cc48217d77e00db63c25241
Abhishek Malvadkar <abhishek@dev.com>
2023-06-17 12:09:37 +0530

    Created simple spring boot project

---
+++ jpa-shoot/src/main/java/com/jpa/shoot/JpaShootApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.shoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShootApplication.class, args);
+	}
+
+}
