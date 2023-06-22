2c0c9ecfb911e51169d440e9241d66bbd03a541a
Abhishek Malvadkar <abhishek@dev.com>
2023-06-22 11:21:01 +0530

    Case 2 : Update with managed object case with @Transactional annotation on current method

--- jpa-shoot/src/main/java/com/jpa/shoot/runner/JpaUpdateInvestigationRunner.java
+++ jpa-shoot/src/main/java/com/jpa/shoot/runner/JpaUpdateInvestigationRunner.java
@@ -4,6 +4,7 @@ import com.jpa.shoot.entity.BlogEntity;
 import com.jpa.shoot.repository.BlogRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
@@ -20,30 +21,48 @@ public class JpaUpdateInvestigationRunner implements CommandLineRunner {
     }
 
     @Override
+    /**
+     * Due to @@Transactional annotation on method level PC will start from method start
+     * amd will end at method end
+     * PC state : []
+     */
     public void run(String... args) throws Exception {
+    }
+
+    @Transactional
+    public void updateCaseWithManagedEntityObjectWithTransactionalAnnotationOnCurrentMethod() {
         Integer blogId = 4;
+
         /**
-         * PC will start at LINE 22 and will end the same line and dbBlogObject will be detached from PC
-         * and will be in detached state
-         * because we didn't put @Transactional annotation on
-         * our run method
+         * This line will fetch blog and put into PC
+         * PC state : [dbBlogObject]
          */
         BlogEntity dbBlogObject = this.em.find(BlogEntity.class, blogId);
+
         /**
-         * This LINE 29 change will not be reflected with UPDATE query to database at the end of transaction
-         * because dbBlogObject is not in PC, or we can say dbBlogObject is not in managed state,
-         * to put it into PC or to make it managed state we have to call merge method explicitly
+         * This line will change the managed blog entity
+         * PC state : [dbBlogObject++] ++ means changed
          */
-        dbBlogObject.setContent("MY new content");
+        dbBlogObject.setContent("MY new content V2");
+
         /**
-         * Here we're putting explicitLY detached object into PC so at the end of transaction if object is changed
-         * then UPDATE query will be fired,
-         * Note : When we are putting detached object into PC then at the end of transaction JPA needs to fire
-         *        one select query to check whether this object is changed or not to take decision on UPDATE query
-         *        fire or not?(This is called dirty checking)
-         *
+         * Here as we know that dbBlogObject is managed, so we don't need to use
+         * merge method explicitly( If you want , you can use, but it does not maje any sense when your entity
+         * object is managed, it will make sense only when your entity object is detached) because when transaction ends then hibernate will
+         * check in the PC that any object value is modified or not ( this type of dirty checking is called PC based dirty checking) and
+         * if and only if it finds that entity any entity object value is changed then only hibernate will fire UPDATE query otherwise
+         * it will not fire UPDATE query, you can see logs
          */
-        this.blogRepository.update(dbBlogObject);
+
+        /**
+         * Conclusions on update
+         * - If you have detached object then mandatory you have to call merge method to put it into PC and in this case at the end of transaction
+         *   SQL based dirty checking will be done where hibernate will fire one select query to check whether this object has
+         *   been changed or not? to take UPDATE query decision
+         * - if you have managed object then using merge method is optional because your object is already available in PC,
+         *   and in this case at the end of transaction PC based dirty checking will be happened where hibernate checks from PC that this object
+         *   is changed or not from its previous state ? to take UPDATE query decision , in this case no extra SQL select query will be executed
+         */
     }
 
     public void updateCaseWithDetachedObjectWithoutTransactionAnnotationOnCurrentMethod() {
