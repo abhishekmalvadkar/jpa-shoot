6d69abdf0bb34496e733631ec8aaeb820abd6895
Abhishek Malvadkar <abhishek@dev.com>
2023-06-22 10:57:34 +0530

    Update Case 1 : Update with fetch and detached object case without @Transactional annotation on current method

--- jpa-shoot/src/main/java/com/jpa/shoot/runner/JpaUpdateInvestigationRunner.java
+++ jpa-shoot/src/main/java/com/jpa/shoot/runner/JpaUpdateInvestigationRunner.java
@@ -45,4 +45,37 @@ public class JpaUpdateInvestigationRunner implements CommandLineRunner {
          */
         this.blogRepository.update(dbBlogObject);
     }
+
+    public void updateCaseWithDetachedObjectWithoutTransactionAnnotationOnCurrentMethod() {
+        Integer blogId = 4;
+        /**
+         * PC will start at LINE 22 and will end the same line and dbBlogObject will be detached from PC
+         * and will be in detached state
+         * because we didn't put @Transactional annotation on
+         * our run method
+         */
+        BlogEntity dbBlogObject = this.em.find(BlogEntity.class, blogId);
+        /**
+         * This LINE 29 change will not be reflected with UPDATE query to database at the end of transaction
+         * because dbBlogObject is not in PC, or we can say dbBlogObject is not in managed state,
+         * to put it into PC or to make it managed state we have to call merge method explicitly
+         */
+        dbBlogObject.setContent("MY new content");
+        /**
+         * Here we're putting explicitLY detached object into PC so at the end of transaction if object is changed
+         * then UPDATE query will be fired,
+         * Note : When we are putting detached object into PC then at the end of transaction JPA needs to fire
+         *        one select query to check whether this object is changed or not to take decision on UPDATE query
+         *        fire or not?(This is called dirty checking)
+         *
+         */
+        this.blogRepository.update(dbBlogObject);
+
+        /**
+         * Conclusions
+         * - If you add detached object into PC then hibernate needs to check whether this object is changed
+         *   or not to take decision on UPDATE query execution and hibernate is checking this by firing one select
+         *   query to database , you can see in log and this type of dirty checking is called SQL query based dirty checking
+         */
+    }
 }
